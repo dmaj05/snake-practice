b64e1d7689cf81f175eb812c58ea8dcb051bc1e3
dmaj05 <vermaabhayy5@gmail.com>
2023-05-06 14:00:12 +0530

    edit4

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ food={x:5, y:5};
 //FUNCTIONS
 function main(currentTime){
     window.requestAnimationFrame(main);
-    console.log(currentTime)
+    //console.log(currentTime)
     if((currentTime-lastPaintTime)/1000 < 1/speed){
         return;
     }
@@ -31,7 +31,12 @@ function Engine(){
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = element.y;
         snakeElement.style.gridColumnStart = element.x;
-        snakeElement.classList.add("head");
+        if(index===0){
+            snakeElement.classList.add("head");
+        }
+        else{
+            snakeElement.classList.add("snake");
+        }
         playArea.appendChild(snakeElement);
     });
 
@@ -48,3 +53,36 @@ function Engine(){
 
 //GAME LOGIC
 window.requestAnimationFrame(main);
+window.addEventListener('keydown', e=>{
+    inputDir={x:0, y:1};
+    switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp")
+            inputDir.x=0 ;
+            inputDir.y=-1;
+            break;
+
+        case "ArrowDown":
+            console.log("ArrowDown")
+            inputDir.x=0;
+            inputDir.y=1;
+            break;
+        
+        case "ArrowLeft":
+            console.log("ArrowLeft")
+            inputDir.x=-1;
+            inputDir.y=0;
+            break;
+        
+        case "ArrowRight":
+            console.log("ArrowRight")
+            inputDir.x=1;
+            inputDir.y=0;   
+            break;
+        default:
+            break;
+    
+        }
+})
+
+
