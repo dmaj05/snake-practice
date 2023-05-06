b12fa113bec1bbf7d04db705ba8d8ca56a32d0fb
dmaj05 <vermaabhayy5@gmail.com>
2023-05-06 12:15:22 +0530

    edit3

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //VARIABLES
-let direction={x:0,y:0};
+let direction={x:0, y:0};
+let speed=2;
+let lastPaintTime=0;
+let snakeArr=[{x:0, y:0}];
 
+food={x:5, y:5};
 
 
 
@@ -9,9 +13,35 @@ let direction={x:0,y:0};
 //FUNCTIONS
 function main(currentTime){
     window.requestAnimationFrame(main);
+    console.log(currentTime)
+    if((currentTime-lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime=currentTime;
+    Engine()
 }
 
+function Engine(){
+    //UPDATING THE SNAKE ARRAY 
 
+
+    //RENDERING THE SNAKE 
+    playArea.innerHTML = "";
+    snakeArr.forEach((element,index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = element.y;
+        snakeElement.style.gridColumnStart = element.x;
+        snakeElement.classList.add("head");
+        playArea.appendChild(snakeElement);
+    });
+
+    //FOOD
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add("food");
+    playArea.appendChild(foodElement);
+}
 
 
 
