9e048fd649182909a8dc7a82dfb9f4aad466c5a4
dmaj05 <vermaabhayy5@gmail.com>
2023-05-06 11:36:54 +0530

    edit2

---
+++ main.js
@@ -0,0 +1,20 @@
+//VARIABLES
+let direction={x:0,y:0};
+
+
+
+
+
+
+//FUNCTIONS
+function main(currentTime){
+    window.requestAnimationFrame(main);
+}
+
+
+
+
+
+
+//GAME LOGIC
+window.requestAnimationFrame(main);
