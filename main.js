3a7845cdcaaf6081c26c3ba79ca67f235d185f7b
dmaj05 <vermaabhayy5@gmail.com>
2023-05-07 12:24:05 +0530

    trial1 pasted

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //VARIABLES
 let inputDir={x:0, y:0};
-let speed=10;
+let speed=8;
 let lastPaintTime=0;
-let snakeArr=[{x:5, y:5}];
+let snakeArr=[{x:10, y:10}];
 let score=0;
-food={x:10, y:5};
+let food={x:10, y:5};
+
+
+
 
 
 
@@ -30,22 +33,33 @@ function isCollide(snake){
             
         }
 
-        if(snake[i].x>=20 || snake[i].x<=0 || snake[i].y>=20 || snake[i].y<=0){
+        if(snake[i].x>20 || snake[i].x<0 || snake[i].y>20 || snake[i].y<0){
             return true;
             
         }
+
+        
+        
     }
+    if(snake[0].x>20 || snake[0].x<0 || snake[0].y>20 || snake[0].y<0){
+      return true;
+      
+  }
 }
 
+
 function Engine(){
-    //UPDATING THE SNAKE ARRAY 
+    
+  
+  //UPDATING THE SNAKE ARRAY 
 
     if(isCollide(snakeArr)){
         inputDir= {x:0, y:0};
         score=0;
         scoreCount.innerHTML="Score:0"
         alert("The Game is Over, press Enter to start again");
-        snakeArr=[{x:5, y:5}];
+        snakeArr=[{x:10, y:10}];
+        food={x:10, y:5}
         
     }
 
@@ -61,15 +75,19 @@ function Engine(){
 
     //FOOD EATING LOGIC
     if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+
+        if(score>=hiscoreval){
+          hiscoreval=score+1;
+          localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+          hiScore.innerHTML = "High Score: " + hiscoreval;
+        }      
         snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y + inputDir.y});
         food = {x:Math.floor((Math.random() * 20) + 1), y:Math.floor((Math.random() * 20) + 1)};
         score+=1;
         scoreCount.innerHTML = "Score:" + score;
         
     } 
-    else {
-        
-    }
+    
 
 
     //RENDERING THE SNAKE 
@@ -93,49 +111,99 @@ function Engine(){
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add("food");
     playArea.appendChild(foodElement);
+    
+    
+    
 
-
-    //GAMEOVER WITH TIMER
-    function gameOver(time){
-        isCollide(snakeArr)=true;
-    }
 }
 
 
 
 
+//GAMEOVER WITH TIMER
+
+
+
 //GAME LOGIC
+
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore===null){
+  let hiscoreval=0;
+  localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+}
+else{
+  hiscoreval=JSON.parse(hiscore);
+  hiScore.innerHTML = "High Score:" + hiscoreval;
+}
+
+
+
+
+
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e=>{
-    inputDir={x:0, y:1};
-    switch(e.key){
+
+    gameStart=true;
+    
+    if(e.key=="ArrowUp" && inputDir.y!=1){
+      inputDir.x=0;
+      inputDir.y=-1;
+      
+    }
+
+    else if(e.key=="ArrowDown" && inputDir.y!=-1){
+      inputDir.x=0;
+      inputDir.y=1;
+      
+    }
+
+    else if(e.key=="ArrowLeft" && inputDir.x!=1){
+      inputDir.x=-1;
+      inputDir.y=0;
+      
+    }
+
+    else if(e.key=="ArrowRight" && inputDir.x!=-1 ){
+      inputDir.x=1;
+      inputDir.y=0;
+      
+    }
+
+
+    
+    
+    /*switch(e.key){
         case "ArrowUp":
             console.log("ArrowUp")
+            
             inputDir.x=0 ;
             inputDir.y=-1;
             break;
 
         case "ArrowDown":
             console.log("ArrowDown")
+            
             inputDir.x=0;
             inputDir.y=1;
             break;
         
         case "ArrowLeft":
             console.log("ArrowLeft")
+            
             inputDir.x=-1;
             inputDir.y=0;
             break;
         
         case "ArrowRight":
             console.log("ArrowRight")
+            
             inputDir.x=1;
             inputDir.y=0;   
             break;
         default:
             break;
     
-        }
+        } */
+
+    
 })
-
-
